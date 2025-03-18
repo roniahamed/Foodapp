b7d4ebe18200e1c09a78f814cb01540aae1a6ee7
roniahamed <mdroniahamed56@gmail.com>
2025-03-18 18:48:20 +0600

    add simple views

--- food/views.py
+++ food/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Hello World")
+    
+
+def work(request):
+    return HttpResponse("This is the work page")
--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-blo!&$tc@@_i#ivfavs@84@!h6kzb$$#cp0+pfn0b*(np!3mxx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'food',
 ]
 
 MIDDLEWARE = [

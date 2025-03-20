22576d1a7914abb4441d6c04342c7e0922411ed3
roniahamed <mdroniahamed56@gmail.com>
2025-03-20 18:01:24 +0600

    update models

--- food/models.py
+++ food/models.py
@@ -4,3 +4,7 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
+
+    def __str__(self):
+        return self.item_name
+    

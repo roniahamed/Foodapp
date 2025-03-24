a65d594611a5b50e0659615484588286d5f15172
roniahamed <mdroniahamed56@gmail.com>
2025-03-25 02:50:27 +0600

    add tamplates

--- food/views.py
+++ food/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Item
+from django.template import loader
 
 def index(request):
     item_list = Item.objects.all()
-    return HttpResponse(item_list)
+    template = loader.get_template('food/index.html')
+    context = {
+        'item_list':item_list
+    }
+    return HttpResponse(template.render(context,request))
     
 
 def item(request):

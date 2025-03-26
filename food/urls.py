100600f6b0ec217a9b2fe3b40888df8057cce871
roniahamed <mdroniahamed56@gmail.com>
2025-03-27 03:51:21 +0600

    Build Update Functionality

--- food/urls.py
+++ food/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('<int:item_id>/',views.detail, name="detail"),
     path('item/', views.item, name='item'),
     # Add item
-    path('add/', views.create_item, name='create_item')
+    path('add/', views.create_item, name='create_item'),
+    # Update Items
+    path('update/<int:id>/', views.update_item, name="update_item"),
 ]

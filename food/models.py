2458510725b1ff882498932196e1e60eb8b45b5b
roniahamed <mdroniahamed56@gmail.com>
2025-03-26 16:43:04 +0600

    add image field

--- food/models.py
+++ food/models.py
@@ -4,6 +4,7 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500, default="https://cdn.vectorstock.com/i/500p/42/11/creative-concept-of-brain-food-symbolized-vector-53434211.jpg")
 
     def __str__(self):
         return self.item_name

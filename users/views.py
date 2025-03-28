2bae4449ae9d73c231165f2b7227f973e9b47386
roniahamed <mdroniahamed56@gmail.com>
2025-03-29 00:45:32 +0600

    register

--- users/views.py
+++ users/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-# Create your views here.
+from .forms import RegisterForm
+
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f"Welcome {username}, Your Account is created!")
             return redirect('food:index')
     else:
-        form = UserCreationForm()
+        form = RegisterForm()
     return render(request,'users/register.html',{'form':form})

82de6ba3be8356400a367b18e289e536e52268e3
roniahamed <mdroniahamed56@gmail.com>
2025-03-29 02:37:24 +0600

    profile url

--- users/urls.py
+++ users/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('register/',views.register, name='register'),
     path('login/',authentication_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/',views.LogoutView, name='logout'),
+    path('profile/',views.profile_page, name='profile'),
 ]

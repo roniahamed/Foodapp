81b3d1ab6a94f5b5d7eac63593d9b8f595cee413
roniahamed <mdroniahamed56@gmail.com>
2025-03-29 04:14:19 +0600

    make Profile model

--- users/models.py
+++ users/models.py
@@ -6,4 +6,6 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='profilepic.jpg', upload_to='profile_pic')
     location = models.CharField(max_length=100)
-    
+
+    def __str__(self):
+        return self.user.username
